a6edf827e508cd53723b6cb09598bd0ff2d8841c
Andrea Rebusti <andrea.rebusti@ippen-digital.de>
2022-12-12 12:24:04 +0100

    test build

--- api/DashboardAPI.tsx
+++ api/DashboardAPI.tsx
@@ -4,6 +4,8 @@ const apiEndpoint = process.env.NEXT_PUBLIC_SETTINGS_API_ENDPOINT || "";
 const apiEndpoint2 = process.env.NEXT_PUBLIC_TESTO || "";
 const apiEndpoint3 = process.env.NEXT_PUBLIC_TESTO_DUE || "";
 const apiEndpoint4 = process.env.NEXT_PUBLIC_TEST || "";
+const apiEndpoint5 = "NEXT_PUBLIC_TEST";
+
 /**
  *
  * @constructor
